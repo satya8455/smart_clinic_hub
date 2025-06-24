bbfba395d093d2cc38d759052fd768d38e07edcc
Abhishek Tripathy <123153903+abhiaz2005@users.noreply.github.com>
2025-06-24 14:58:37 +0530

    @Initial-commit

---
+++ SmartClinicHub/src/main/java/com/sch/SmartClinicHubApplication.java
@@ -0,0 +1,13 @@
+package com.sch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartClinicHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartClinicHubApplication.class, args);
+	}
+
+}
