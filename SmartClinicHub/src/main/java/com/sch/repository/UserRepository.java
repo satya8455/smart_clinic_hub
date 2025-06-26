7f605557ae22d93804698fea5220b596393d8ea4
Subhashree-68 <123484007+Subhashree-68@users.noreply.github.com>
2025-06-26 11:32:49 +0530

    update

--- SmartClinicHub/src/main/java/com/sch/repository/UserRepository.java
+++ SmartClinicHub/src/main/java/com/sch/repository/UserRepository.java
@@ -5,11 +5,15 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.sch.entity.Clinic;
 import com.sch.entity.User;
+import com.sch.enums.Role;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 	Optional<User> findByEmail(String username);
+
+	Optional<User> findByRoleAndClinic(Role role, Clinic clinic);
 
 }
 	
